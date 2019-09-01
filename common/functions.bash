83afbb26bc38a2497a346d8554ffd91d85e6de32
Joachim Meyer <jomey@uw.edu>
2019-08-31 20:40:38 -0700

    functions.bash - Add ssh completion
    Add ability to complete ssh hosts with tab key.
    The hosts are defined in the ssh/config file.

--- common/functions.bash
+++ common/functions.bash
@@ -2,6 +2,12 @@ function parse_git_branch () {
    git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'
 }
 
+# SSH Host config completion
+function _ssh_completion() {
+ perl -ne 'print "$1 " if /^Host (.+)$/' ~/.ssh/config
+}
+complete -W "$(_ssh_completion)" ssh scp
+
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
 __conda_setup="$("${CONDA_HOME}/bin/conda" 'shell.bash' 'hook' 2> /dev/null)"
