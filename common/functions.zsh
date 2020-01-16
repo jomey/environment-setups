a0d8e1b45ae6a420e9b84ba873dadea39f9caa34
Joachim Meyer <j.meyer@utah.edu>
2020-01-15 19:34:32 -0700

    Z-shell - Add first round of configuration.

---
+++ common/functions.zsh
@@ -0,0 +1,20 @@
+function start_jupyter_lab() {
+  conda activate jupyter-lab
+  cd ${PROJECTS_ROOT}
+  jupyter lab
+}
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+__conda_setup="$("${CONDA_HOME}/bin/conda" 'shell.zsh' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "${CONDA_HOME}/etc/profile.d/conda.sh" ]; then
+        . "${CONDA_HOME}/etc/profile.d/conda.sh"
+    else
+        export PATH="${CONDA_HOME}/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+# <<< conda initialize <<<
