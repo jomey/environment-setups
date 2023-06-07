22720c3dfac36bbb82931c0c9e715fe6a420e2b3
Joachim Meyer <jomey@uw.edu>
2023-06-07 14:38:33 -0600

    Bash - Micromamba - Slight syntax improvement

--- common/micromamba.sh
+++ common/micromamba.sh
@@ -1,16 +1,18 @@
 #!/usr/bin/env bash
 
 # >>> mamba initialize >>>
+# !! Contents within this block are managed by 'mamba init' !!
 export MAMBA_EXE="/data/micromamba/bin/micromamba";
 export MAMBA_ROOT_PREFIX="/data/micromamba";
-__mamba_setup="$('/data/micromamba/bin/micromamba' shell hook --shell bash --prefix '/data/micromamba' 2> /dev/null)"
+__mamba_setup="$("$MAMBA_EXE" shell hook --shell bash --prefix "$MAMBA_ROOT_PREFIX" 2> /dev/null)"
 if [ $? -eq 0 ]; then
   eval "$__mamba_setup"
 else
   if [ -f "/data/micromamba/etc/profile.d/micromamba.sh" ]; then
     . "/data/micromamba/etc/profile.d/micromamba.sh"
   else
-    export  PATH="$PATH:/data/micromamba/bin"  # extra space after export prevents interference from conda init
+    # extra space after export prevents interference from conda init
+    export  PATH="$PATH:/data/micromamba/bin"
   fi
 fi
 unset __mamba_setup
