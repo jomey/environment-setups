48c07940427c49749fd9d31b0623860eadaf4e8c
Joachim Meyer <jmeyer@boisestate.edu>
2024-03-25 10:36:24 -0600

    Common - Add function to parse conda env
    Used to modify the PS1 prompt

--- common/conda.sh
+++ common/conda.sh
@@ -7,3 +7,12 @@ eval "$(/data/miniconda3/bin/conda  'shell.bash' 'hook')"
 function cl() {
   conda activate ${1}
 }
+
+# For prompt modifications
+function parse_conda_env () {
+    if [ ! -z "$CONDA_DEFAULT_ENV" ]
+    then
+        echo "<$(basename "$CONDA_DEFAULT_ENV")>"
+    fi
+}
+
