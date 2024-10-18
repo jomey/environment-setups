df968ecc821cb403c9bca54aa7816ed4a5125545
Joachim Meyer <jmeyer@boisestate.edu>
2024-10-18 13:16:18 -0600

    Common - Update micromamba init

--- common/micromamba.sh
+++ common/micromamba.sh
@@ -2,18 +2,13 @@
 
 # >>> mamba initialize >>>
 # !! Contents within this block are managed by 'mamba init' !!
-export MAMBA_EXE="/data/micromamba/bin/micromamba";
-export MAMBA_ROOT_PREFIX="/data/micromamba";
-__mamba_setup="$("$MAMBA_EXE" shell hook --shell bash --prefix "$MAMBA_ROOT_PREFIX" 2> /dev/null)"
+export MAMBA_EXE='/data/micromamba/bin/micromamba';
+export MAMBA_ROOT_PREFIX='/data/micromamba';
+__mamba_setup="$("$MAMBA_EXE" shell hook --shell bash --root-prefix "$MAMBA_ROOT_PREFIX" 2> /dev/null)"
 if [ $? -eq 0 ]; then
-  eval "$__mamba_setup"
+    eval "$__mamba_setup"
 else
-  if [ -f "/data/micromamba/etc/profile.d/micromamba.sh" ]; then
-    . "/data/micromamba/etc/profile.d/micromamba.sh"
-  else
-    # extra space after export prevents interference from conda init
-    export  PATH="$PATH:/data/micromamba/bin"
-  fi
+    alias micromamba="$MAMBA_EXE"  # Fallback on help from micromamba activate
 fi
 unset __mamba_setup
 # <<< mamba initialize <<<
