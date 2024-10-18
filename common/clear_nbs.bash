6d4077d3220c4d6acd9c1c37de7205d8672ccd0e
Joachim Meyer <jmeyer@boisestate.edu>
2024-10-18 13:16:37 -0600

    Common - Update command to clear notebooks

--- common/clear_nbs.bash
+++ common/clear_nbs.bash
@@ -1,15 +1,10 @@
 #!/usr/bin/env bash
 
 function clear_nbs() {
-    if [[ -x 'jupyter' ]]; then
-      command=jupyter
-    else
-      command=${CONDA_PREFIX}/bin/jupyter
-    fi
-
-    $command nbconvert \
-      --ClearMetadataPreprocessor.enabled=True \
-      --clear-output --log-level=ERROR \
-      ${1:-*.ipynb}
+  if ! $(type jupyter &> /dev/null); then
+    echo "Required jupyter command not found"
+  else
+    jupyter  nbconvert --clear-output --inplace --log-level=ERROR $1
+  fi
 }
 
