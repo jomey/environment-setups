51650b92d7d8b1608db98b145dab9c6f77931eef
Joachim Meyer <jmeyer@scitenia.org>
2025-11-13 09:14:19 -0700

    Common - use `micromamba run` for alias calls
    No need to activate environments

--- common/clear_nbs.bash
+++ common/clear_nbs.bash
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 
 function clear_nbs() {
-  if ! $(type jupyter &> /dev/null); then
-    echo "Required jupyter command not found"
-  else
-    jupyter  nbconvert --clear-output --inplace --log-level=ERROR $1
-  fi
+  micromamba run -n jupyter-lab jupyter  nbconvert --clear-output --inplace --log-level=ERROR $1
 }
 
