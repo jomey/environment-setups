df18bed0ce008188f15eec2b5ecf7048505b5972
Joachim Meyer <jmeyer@boisestate.edu>
2025-11-13 09:10:36 -0700

    Common - Remove miniconda init call

--- common/conda.sh
+++ common/conda.sh
@@ -1,11 +1,8 @@
 #!/usr/bin/env bash
 
-# Initialize conda
-eval "$(/data/miniconda3/bin/conda  'shell.bash' 'hook')"
-
 # Shortcut to load environments
 function cl() {
-  conda activate ${1}
+  micromamba activate ${1}
 }
 
 # For prompt modifications
