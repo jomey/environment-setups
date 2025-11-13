5abfcdfd7e8f00f8309137904c77830949e7cb10
Joachim Meyer <jmeyer@scitenia.org>
2025-11-13 09:15:38 -0700

    Common - Imports - Add spack activate init

--- common/imports.bash
+++ common/imports.bash
@@ -4,6 +4,7 @@ source ${PROJECTS_ROOT}/environment-setups/common/functions.bash
 source ${PROJECTS_ROOT}/environment-setups/common/conda.sh
 source ${PROJECTS_ROOT}/environment-setups/common/micromamba.sh
 source ${PROJECTS_ROOT}/environment-setups/common/clear_nbs.bash
+source ${PROJECTS_ROOT}/spack/share/spack/setup-env.sh
 
 export GDAL_OPTS=${PROJECTS_ROOT}/environment-setups/common/gdal_opts.txt
 
